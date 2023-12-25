278983cc7fe2c96f16df1d1f4896103a1a40848b
Aimen <mizou9999@yandex.ru>
2023-12-25 21:37:37 +0300

    restored dist folder

---
+++ dist/assets/main-YnUbawrP.js
@@ -0,0 +1,30 @@
+import"./modulepreload-polyfill-9p4a8sJU.js";document.querySelector("#app").innerHTML=`
+    <nav class="nav">
+    <ul>
+        
+        <li><a href="/pages/login/login.html">Вход</a></li>
+        <li><a href="/pages/registration/registration.html"> Регистрация</a></li>
+        <li><a href="/pages/chat/chat.html"> Список чатов</a></li>
+        <li><a href="/pages/profile/settings.html"> Настройки пользователя</a></li>
+        <li><a href="/pages/not_found/400.html"> 400</a></li>
+        <li><a href="/pages/not_found/500.html"> 500</a></li>
+    </ul>
+        
+    </nav>
+    <div class="home">
+        <div class="home__svg-container">
+            
+        </div>
+        <h1 class='home__title'>Yandex.Practicum Messenger</h1>
+        <div class='home__nav'>
+        <ul>
+        <li><a href="/pages/login/login.html">Вход</a></li>
+        <li><a href="/pages/registration/registration.html"> Регистрация</a></li>
+        <li><a href="/pages/chat/chat.html"> Список чатов</a></li>
+        <li><a href="/pages/profile/settings.html"> Настройки пользователя</a></li>
+        <li><a href="/pages/not_found/400.html"> 400</a></li>
+        <li><a href="/pages/not_found/500.html"> 500</a></li>
+        </ul>
+        </div>
+    </div>
+`;
